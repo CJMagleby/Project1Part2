9bec604dd13bde68c11c4c37fc337925fd095a1a
CJ Magleby <cjmagleby@gmail.com>
2018-10-03 20:49:53 -0700

    Initial Commit

---
+++ Project1Part2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project1Part2
+//
+//  Created by CJ Magleby on 10/3/18.
+//  Copyright © 2018 CJ Magleby. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
